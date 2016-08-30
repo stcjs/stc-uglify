268f581a1061ae077904088569ee5ed7a5fffcf7
welefen <welefen@gmail.com>
2016-08-30 09:43:22 +0800

    use uglify 1.x instead

--- src/index.js
+++ src/index.js
@@ -12,65 +12,12 @@ export default class JSCompressPlugin extends Plugin {
     }
     
     let content = await this.getContent('utf8');
-    let options = this.options;
-    let ast;
-    
-    try{
-      ast = UglifyJS.parse(content, {
-        filename: this.file.path
-      });
-    }catch(e){
-      return {err: {
-        message: e.message,
-        line: e.line,
-        col: e.col
-      }};
-    }
-    
-    if(options.compress !== false) {
-      ast.figure_out_scope();
-      // eslint-disable-line new-cap
-      let compress = UglifyJS.Compressor(options.compress || { 
-        warnings: false,
-        screw_ie8: false
-      });
-      ast = ast.transform(compress);
-    }
-    
-    if(options.mangle !== false) {
-      ast.figure_out_scope();
-      ast.compute_char_frequency(options.mangle || {
-        screw_ie8: false
-      });
-      ast.mangle_names(options.mangle || {
-        screw_ie8: false
-      });
-      if(options.mangle && options.mangle.props) {
-        UglifyJS.mangle_properties(ast, options.mangle.props);
-      }
-    }
-    
-    //output options
-    let output = {
-      comments: options.comments
-    };
-    if(!Object.prototype.hasOwnProperty.call(options, 'comments')){
-      output.comments = /^\**!|@preserve|@license/;
-    }
-    output = extend(output, options.output);
-    if(options.sourceMap){
-      output.source_map = UglifyJS.SourceMap({
-        file: this.file.path,
-        root: ''
-      });
-    }
-    output.screw_ie8 = false;
-    // eslint-disable-line new-cap
-    let stream = UglifyJS.OutputStream(output); 
-    ast.print(stream);
-    let code = stream.toString();
-    let sourceMap = output.source_map && output.source_map.toString();
-    return {content: code, sourceMap};
+    let {parser, uglify} = UglifyJS;
+    let ast = parser.parse(content); 
+    ast = uglify.ast_mangle(ast); // get a new AST with mangled names
+    ast = uglify.ast_squeeze(ast); // get an AST with compression optimizations
+    let ret = uglify.gen_code(ast); // compressed code here
+    return {content: ret};
   }
   /**
    * update
